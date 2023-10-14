bc17712867f6448a60d6a329758e8ddd46cee3ce
Anavadya Anil <anavadyaanil669@gmail.com>
2023-10-14 11:17:24 +0530

    Customer list code changed.

--- resources/customer_list.php
+++ resources/customer_list.php
@@ -1,3 +1,20 @@
+<?php
+include "header.php";
+include "user_navbar.php";
+include "connection.php";
+
+
+
+$customer_sql = "SELECT c.*, a.balance FROM customer_table c
+                 LEFT JOIN accounts_table a ON c.user_id = a.user_id"; // Modify the query to join tables
+
+$result = $conn->query($customer_sql);
+if ($result === false) {
+    echo "Error: " . $conn->error; // Display any database query errors
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en-GB">
 
@@ -25,63 +42,29 @@
                 <th>Branch</th>
                 <th>Join Date & time</th>
                 <th>Pin</th>
+                <th>Balance</th> <!-- New column for role -->
             </tr>
         </thead>
         <tbody>
             <?php
-            include "header.php";
-            include "user_navbar.php";
-            include "connection.php";
-
-            $customer_sql = "SELECT * FROM customer_table";
-            $result = $conn->query($customer_sql);
             if ($result->num_rows > 0) {
-                // while ($row = $result->fetch_assoc()) {
-                //     echo "   " . $row["first_name"];
-
-
-
-                    //
-            
-
-
-                    // Replace this with code to fetch and display customer data from your database
-                    $customers = [
-                        [
-                            'first_name' => 'John',
-                            'last_name' => 'Doe',
-                            'address' => '123 Main St',
-                            'contact' => '123-456-7890',
-                            'email' => 'john@example.com',
-                            'gender' => 'Male',
-                            'dob' => '1990-01-15',
-                            'pan_card' => 'ABCDE1234F',
-                            'aadhar_no' => '1234 5678 9012',
-                            'branch' => 'XYZ Bank',
-                            'join_date' => '2022-05-20',
-                            'pin' => '12345',
-                        ],
-                        // Add more customer data as needed
-                    ];
+                while ($row = $result->fetch_assoc()) {
+                    echo "<tr>";
+                    echo "<td>{$row["first_name"]}</td>";
+                    echo "<td>{$row["last_name"]}</td>";
+                    echo "<td>{$row["address"]}</td>";
+                    echo "<td>{$row["contact_no"]}</td>";
+                    echo "<td>{$row["email_id"]}</td>";
+                    echo "<td>{$row["gender"]}</td>";
+                    echo "<td>{$row["dob"]}</td>";
+                    echo "<td>{$row["pan_card_no"]}</td>";
+                    echo "<td>{$row["aadhaar_no"]}</td>";
+                    echo "<td>{$row["branch"]}</td>";
+                    echo "<td>{$row["join_date"]}</td>";
+                    echo "<td>{$row["pin"]}</td>";
+                    echo "<td>{$row["balance"]}</td>"; // Display the role from the login_table
+                    echo "</tr>";
                 }
-
-        //    }
-
-            while ($row = $result->fetch_assoc()) {
-                echo "<tr>";
-                 echo "<td>{$row["first_name"]}</td>";
-                 echo "<td>{$row["last_name"]}</td>";
-                 echo "<td>{$row["address"]}</td>";
-                 echo "<td>{$row["contact_no"]}</td>";
-                 echo "<td>{$row["email_id"]}</td>";
-                 echo "<td>{$row["gender"]}</td>";
-                 echo "<td>{$row["dob"]}</td>";
-                 echo "<td>{$row["pan_card_no"]}</td>";
-                 echo "<td>{$row["aadhaar_no"]}</td>";
-                 echo "<td>{$row["branch"]}</td>";
-                 echo "<td>{$row["join_date"]}</td>";
-                 echo "<td>{$row["pin"]}</td>";
-                echo "</tr>";
             }
             ?>
         </tbody>
