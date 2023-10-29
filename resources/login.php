8ea1685e1264f1afdc64b19c706fd3a0cc4a2399
Ajay ps <ajaypalamkunnel45@gmail.com>
2023-10-29 15:22:58 +0530

    login issue after encryption fixed

--- resources/login.php
+++ resources/login.php
@@ -1,9 +1,11 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-<link rel="stylesheet" href="style/login_style.css">
+    <link rel="stylesheet" href="style/login_style.css">
     <title>Login to Bank</title>
 </head>
+
 <body>
     <div class="loginform">
         <center>
@@ -25,70 +27,73 @@
         </center>
     </div>
     <?php
-session_start(); // Start the session
-require("connection.php");
+    session_start(); // Start the session
+    require("connection.php");
 
-if (isset($_POST["login"])) {
-    $n = $_POST['username'];
-    $p = $_POST['password'];
-    $r = $_POST['role'];
-    
+    if (isset($_POST["login"])) {
+        $n = $_POST['username'];
+        $p = $_POST['password'];
+        $r = $_POST['role'];
 
-    // Use prepared statement to fetch the hashed password from the database
-    $sql = "SELECT user_id, password, role FROM login_table WHERE user_id=?";
-    $stmt = mysqli_prepare($conn, $sql);
-    mysqli_stmt_bind_param($stmt, "s", $n);
-    mysqli_stmt_execute($stmt);
-    $result = mysqli_stmt_get_result($stmt);
 
-    if (mysqli_num_rows($result) > 0) {
-        $user_info = mysqli_fetch_assoc($result);
-        $hashedPassword = $user_info['password'];
+        // Use prepared statement to fetch the hashed password from the database
+        $sql = "SELECT user_id, password, role FROM login_table WHERE user_id=?";
+        $stmt = mysqli_prepare($conn, $sql);
+        mysqli_stmt_bind_param($stmt, "s", $n);
+        mysqli_stmt_execute($stmt);
+        $result = mysqli_stmt_get_result($stmt);
 
-        // if($r=='admin') {
-        
-        // }
+        if (mysqli_num_rows($result) > 0) {
+            $user_info = mysqli_fetch_assoc($result);
+            $pass = $user_info['password'];
+            $hashedPassword = $user_info['password'];
 
-        // Use password_verify to check if the entered password matches the stored hash
-        if (password_verify($p, $hashedPassword)) {
-            // Password is correct
-
-            // Store user information in session variables
-            $_SESSION['user_id'] = $user_info['user_id'];
-            $_SESSION['role'] = $user_info['role'];
-
-            // Fetch the account_no based on user_id
-            $sql = "SELECT account_no FROM accounts_table WHERE user_id=?";
-            $stmt = mysqli_prepare($conn, $sql);
-            mysqli_stmt_bind_param($stmt, "s", $_SESSION['user_id']);
-            mysqli_stmt_execute($stmt);
-            $account_result = mysqli_stmt_get_result($stmt);
-
-            if (mysqli_num_rows($account_result) > 0) {
-                $account_info = mysqli_fetch_assoc($account_result);
-                $_SESSION['account_no'] = $account_info['account_no'];
-            }
-
-            if ($r === 'Admin') {
+            if ($r == 'Admin' && $p == $pass) {
+                $_SESSION['role'] = $user_info['role'];
                 header("Location: admin_dashboard.php");
                 exit();
-            } else if ($r === 'Customer') {
-                header("Location: customer_dashboard.php");
-                exit();
+
             } else {
-                // Invalid role, handle as needed
-                echo "Invalid role!";
+
+                // Use password_verify to check if the entered password matches the stored hash
+                if (password_verify($p, $hashedPassword)) {
+                    // Password is correct
+    
+                    // Store user information in session variables
+                    $_SESSION['user_id'] = $user_info['user_id'];
+                    $_SESSION['role'] = $user_info['role'];
+
+                    // Fetch the account_no based on user_id
+                    $sql = "SELECT account_no FROM accounts_table WHERE user_id=?";
+                    $stmt = mysqli_prepare($conn, $sql);
+                    mysqli_stmt_bind_param($stmt, "s", $_SESSION['user_id']);
+                    mysqli_stmt_execute($stmt);
+                    $account_result = mysqli_stmt_get_result($stmt);
+
+                    if (mysqli_num_rows($account_result) > 0) {
+                        $account_info = mysqli_fetch_assoc($account_result);
+                        $_SESSION['account_no'] = $account_info['account_no'];
+                    }
+
+                    if ($r === 'Customer') {
+                        header("Location: customer_dashboard.php");
+                        exit();
+                    } else {
+                        // Invalid role, handle as needed
+                        echo '<script>alert("Invalid role")</script>';
+                    }
+                } else {
+                    echo '<script>alert("Invalid password")</script>';
+                }
             }
         } else {
-            echo '<script>alert("Invalid password")</script>';
+            echo '<script>alert("Invalid login credentials")</script>';
         }
-    } else {
-        echo '<script>alert("Invalid login credentials")</script>';
-    }
 
-    mysqli_stmt_close($stmt);
-}
-?>
+        mysqli_stmt_close($stmt);
+    }
+    ?>
 
 </body>
+
 </html>
