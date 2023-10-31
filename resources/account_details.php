165cda2d2d817884fc728531d73b5d8290f21a43
Anavadya Anil <anavadyaanil669@gmail.com>
2023-10-31 21:06:40 +0530

    Admin dashboard modified.

--- resources/account_details.php
+++ resources/account_details.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="style/customer_profile_admin_style.css">
-    <title>Customer Details</title>
+    <title>Account Details</title>
 </head>
 
 <body>
